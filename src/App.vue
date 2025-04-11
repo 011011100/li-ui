2f3a1041263f6a4d4cb742155a7db042f01cac82
LZH <LZH01101110@163.com>
2025-04-11 16:26:28 +0800

    feat: 测试

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import type {liItemProps} from "./components/menu/li-menu-type.ts";
+import LiMenu from "./components/menu/li-menu.tsx";
+
+const liItem = ref<liItemProps[]>([
+  {
+    name: '1',
+    content: '111',
+    children: [
+      {
+        name: '1-1',
+        content: '222',
+        children: [
+          {
+            name: '1-1-1',
+            content: '333'
+          }
+        ]
+      },
+      {
+        name: '1-2',
+        content: '222',
+        children: [
+          {
+            name: '1-2-1',
+            content: '333'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: '2',
+    content: '123',
+    children: [
+      {
+        name: '2-1',
+        content: '1233',
+        children: [
+          {
+            name: '2-1-1',
+            content: '12333'
+          }
+        ]
+      }
+    ]
+  }
+])
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <LiMenu :li-item="liItem" style="width: 200px;"/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
