7f88bb78a90e6e3977c037b7f579bd1efbf5b40e
LZH <LZH01101110@163.com>
2025-04-11 16:24:58 +0800

    fix: 调整配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vueJsx(),
-    AutoImport({
-      // 你可以指定要自动导入的库
-      imports: [
-        'vue',
-        // ...其他库
-      ],
-    }),
-    vue()
-  ],
+    plugins: [
+        vueJsx(),
+        AutoImport({
+            // 你可以指定要自动导入的库
+            imports: [
+                'vue',
+                // ...其他库
+            ],
+        }),
+        Components({
+            dts: true,
+            include: [/\.vue$/, /\.vue\?vue/, /\.vue\.[tj]sx?\?vue/, /\.md$/],
+        }),
+        vue()
+    ],
 })
