667a8fd28885c4dbf2437bd0903ad198e1bb36ea
LZH <LZH01101110@163.com>
2025-04-27 10:41:23 +0800

    feat: 抽出css样式

--- src/components/menu/li-menu.tsx
+++ src/components/menu/li-menu.tsx
@@ -1,6 +1,7 @@
-import type {CSSProperties, PropType} from "vue";
+import type {PropType} from "vue";
 import {defineComponent, ref, Transition, TransitionGroup} from "vue";
 import type {liItemProps} from "./li-menu-type.ts";
+import './li-menu.css'
 
 const LiMenu = defineComponent({
     name: 'LiMenu',
@@ -25,49 +26,27 @@ const LiMenu = defineComponent({
             menuStateMap.value[name] = !menuStateMap.value[name]
         }
 
-        const ulStyle: CSSProperties = {
-            listStyle: 'none',
-            padding: 'unset',
-            margin: 'unset',
-        };
-
-        const liItemDivStyle: CSSProperties = {
-            paddingLeft: '8px'
-        };
-
-        const liDivStyle: CSSProperties = {
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center'
-        }
-
         expose({
-            liDivStyle,
             menuStateMap,
-            ulStyle,
-            liItemDivStyle,
             toggleMenu
         });
 
         return {
-            liDivStyle,
             menuStateMap,
-            ulStyle,
-            liItemDivStyle,
             toggleMenu
         }
     },
     render() {
         return <TransitionGroup name="list" tag="div">
             {this.liItem.map((item: any) => (
-                <ul style={this.ulStyle}
+                <ul class={'li-ul'}
                     key={item}>
-                    <li style={this.liItemDivStyle}
+                    <li class={'li-item'}
                         onClick={(e) => {
                             e.stopPropagation()
                             this.toggleMenu(item)
                         }}>
-                        <div style={this.liDivStyle}>
+                        <div class={'li-item-div'}>
                             {this.collapse === false ?
                                 <>
                                     {item.comp ?
@@ -92,7 +71,7 @@ const LiMenu = defineComponent({
                                             <span>
                                                 {
                                                     item.img ?
-                                                        <img style={{paddingRight: '8px'}}
+                                                        <img class={'li-item-div-img'}
                                                              src={item.img}
                                                              alt={item.name}
                                                              width={12}
@@ -120,7 +99,7 @@ const LiMenu = defineComponent({
                                 </> :
                                 <>
                                     {item.img ?
-                                        <img style={{paddingRight: '8px'}}
+                                        <img class={'li-item-div-img'}
                                              src={item.img}
                                              alt={item.name}
                                              width={12}
